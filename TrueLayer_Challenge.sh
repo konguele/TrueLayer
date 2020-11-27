de44a8b21ed65c84b3f7fa84a04443b59f3662b9
konguele <juanjo.gmanzano@gmail.com>
2020-11-27 18:19:46 +0100

    TrueLayer Exercise

--- TrueLayer_Challenge.sh
+++ TrueLayer_Challenge.sh
@@ -17,7 +17,6 @@ fi
 #This step is not necessary because it is created automatically when you output the information to a file, but I add it as a point in the exercise. (Create file)
 touch $folder/TLTest.txt
 
-echo "" >> $folder/TLTest.txt
 echo "------------------------ START EXECUTION $day ------------------------" >> $folder/TLTest.txt
 echo "Time when the script has been run: $time" >> $folder/TLTest.txt
 echo "Name of the script: " $BASH_SOURCE >> $folder/TLTest.txt
@@ -30,3 +29,4 @@ echo "Full list files and folders: " `$runningfolder | ls *` >> $folder/TLTest.t
 
 
 echo "--------------------------------------- END -----------------------------------------" >> $folder/TLTest.txt
+echo "" >> $folder/TLTest.txt
