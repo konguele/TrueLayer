8fb0e47e0e7237930708cace77a798c55a8b2ddb
konguele <juanjo.gmanzano@gmail.com>
2020-11-30 09:59:54 +0100

    Activities

--- exercise4.sh
+++ exercise4.sh
@@ -1,66 +1,84 @@
 #!/bin/bash
-#script_exercise4.sh
-#created by Juanjo Garcia
+# script_exercise4.sh
+# created by Juanjo Garcia
 
-SCRIPT=script_exercise4.sh         #Name of the file to generate.
-FOLDER=/tmp/TrueLayer              #We create the variable of the directory on which we are going to work
+SCRIPT=script_exercise4.sh         # Name of the file to generate.
+FOLDER=/tmp/TrueLayer              # We create the variable of the directory on which we are going to work
 
-#We check if it exists, if it does not exist it will create it
-if [ ! -d "$FOLDER" ]
+echo "Would you like to try exercise 4 with your own path or use the demo?"
+echo ""
+echo "1) Your own path"
+echo "2) Demo"
+echo "3) Exit"
+
+read option
+
+case $option in
+        1)
+         echo "Tell me your folder (Please specify complete path):"
+         read FOLDER
+         ;;
+        2)
+         #We check if it exists, if it does not exist it will create it
+         if [ ! -d "$FOLDER" ]
+         then
+                mkdir -p "$FOLDER"
+                touch $FOLDER/file1.txt
+                echo -n $'\x01' > $FOLDER/binary.dat
+                mkdir -p $FOLDER/TrueLayer2
+                touch $FOLDER/TrueLayer2/file200.txt
+                echo -n $'\x01' > $FOLDER/TrueLayer2/binary_10.dat
+         fi
+         ;;
+        3)
+         echo "OK, see you!"
+         exit 0
+         ;;
+        *)
+         echo "That is not a valid choice"
+         exit 0
+         ;;
+esac
+
+cat << CONVERT
+
+`find $FOLDER -type f > /tmp/form.txt`
+
+The form with the data has been generated
+
+CONVERT
+
+(
+cat << 'EOF'
+#!/bin/bash
+#Script created by Juanjo Garcia
+
+NEW_LOCATION=/tmp/backup/
+
+echo "This is a generated shell script by exercise4.sh"
+
+echo "I start to run the script: $SCRIPT"
+
+if [ ! -d "$NEW_LOCATION" ]
 then
-        mkdir -p "$FOLDER"
-        touch $FOLDER/file1.txt
-        echo -n $'\x01' > $FOLDER/binary.dat
-        mkdir -p $FOLDER/TrueLayer2
-        touch $FOLDER/TrueLayer2/file200.txt
-        echo -n $'\x01' > $FOLDER/TrueLayer2/binary_10.dat
+        mkdir -p "$NEW_LOCATION"
 fi
 
-#We convert the folder hierarchy into a form using cat EOF
-cat << converttoform
-`ls -R $FOLDER > /tmp/form.txt`
-converttoform
-
-#Seguramente tenga que usar un bucle (While read) y el comando find. Así me busca dónde está y lo coloca dentro de la nueva ubicación, las carpetas puede crearlas de nuevo
-#We create the script that will process the information and will generate the folder hierarchy in a new path
-(
-cat << EOF
-#!/bin/bash
-
-echo "This is a generated shell script."
-#  Note that since we are inside a subshell,
-#+ we can't access variables in the "outside" script.
-
-echo "Generated file will be named: $OUTFILE"
-#  Above line will not work as normally expected
-#+ because parameter expansion has been disabled.
-#  Instead, the result is literal output.
-
-a=7
-b=3
-
-let "c = $a * $b"
-echo "c = $c"
+while read line; do
+cp -r -a $line $NEW_LOCATION
+echo "Funciona"
+done < /tmp/form.txt
 
 exit 0
 EOF
-) > $OUTFILE
-# -----------------------------------------------------------
+) > $SCRIPT
 
-#  Quoting the 'limit string' prevents variable expansion
-#+ within the body of the above 'here document.'
-#  This permits outputting literal strings in the output file.
-
-if [ -f "$OUTFILE" ]
+if [ -f "$SCRIPT" ]
 then
-  chmod 755 $OUTFILE
+  chmod 755 $SCRIPT
   # Make the generated file executable.
 else
-  echo "Problem in creating file: \"$OUTFILE\""
+  echo "Problem in creating file:" $SCRIPT
 fi
 
-#  This method also works for generating
-#+ C programs, Perl programs, Python programs, Makefiles,
-#+ and the like.
-
-exit 0
+sh $SCRIPT
