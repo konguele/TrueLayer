14083c60964f1865b92fd62de36698b903542745
konguele <juanjo.gmanzano@gmail.com>
2020-11-30 17:16:29 +0100

    Activities

--- exercise4.sh
+++ exercise4.sh
@@ -2,8 +2,20 @@
 # script_exercise4.sh
 # created by Juanjo Garcia
 
-SCRIPT=script_exercise4.sh         # Name of the file to generate.
+SCRIPT=/tmp/script_exercise4.sh         # Name of the file to generate.
+
+#We start the cat EOF so that it creates the shell script
+(
+cat << 'EOF'
+#!/bin/bash
+#Script created by Juanjo Garcia
+
+echo "This is a generated shell script by exercise4.sh"
+
 FOLDER=/tmp/TrueLayer              # We create the variable of the directory on which we are going to work
+NEW_LOCATION=/tmp/backup
+
+#I have created a small form to be able to choose different ways to do it. Giving a path or using the demo
 
 echo "Would you like to try exercise 4 with your own path or use the demo?"
 echo ""
@@ -40,38 +52,44 @@ case $option in
          ;;
 esac
 
-cat << CONVERT
+#After doing the first part, we put the files from the chosen path in a form. We do the same with the folder hierarchy
 
-`find $FOLDER -type f > /tmp/form.txt`
+find $FOLDER -type f > /tmp/form.txt
+ls -R $FOLDER | grep ":" | awk -F $FOLDER '{print $2}' | cut -d":" -f1 | grep -v ^$ > /tmp/folderhierarchy.txt
 
-The form with the data has been generated
+echo "The form with the data has been generated"
 
-CONVERT
-
-(
-cat << 'EOF'
-#!/bin/bash
-#Script created by Juanjo Garcia
-
-NEW_LOCATION=/tmp/backup/
-
-echo "This is a generated shell script by exercise4.sh"
-
-echo "I start to run the script: $SCRIPT"
+#We create the new location, in this case called backup inside / tmp
 
 if [ ! -d "$NEW_LOCATION" ]
 then
         mkdir -p "$NEW_LOCATION"
+        mkdir -p "$NEW_LOCATION$FOLDER"
+        LOCATION="$NEW_LOCATION$FOLDER"
+else
+        mkdir -p "$NEW_LOCATION$FOLDER"
+        LOCATION="$NEW_LOCATION$FOLDER"
 fi
 
-while read line; do
-cp -r -a $line $NEW_LOCATION
-echo "Funciona"
+#The two while loops are used to recreate the hierarchy and copy the files to the correct place
+
+while read line
+do
+        mkdir -p $LOCATION/$line
+
+done < /tmp/folderhierarchy.txt
+
+while read line
+do
+        cp -r -a $line $NEW_LOCATION$line
+
 done < /tmp/form.txt
 
 exit 0
 EOF
 ) > $SCRIPT
+
+#Once the EOF is finished, we check that the generated script has the correct permissions and we execute it
 
 if [ -f "$SCRIPT" ]
 then
